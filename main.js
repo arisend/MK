0d2168642205995a42abaa5c9a64814c6f1a9e35
arisend <64369075+arisend@users.noreply.github.com>
2021-06-17 21:29:36 +0300

    homework-7(update)

--- main.js
+++ main.js
@@ -1,87 +1,7 @@
 
-import {renderLogs,enemyAttack,playerAttack, createElement,getRandom,createReloadButton,playerWins,appendReloadB} from './utils.js'
-import {logs} from './logs.js'
-function generateLogs(type,player1,player2,value){
-	const date = new Date();
-	console.log(player2)
-	switch (type){
-		case "hit":
-			return date.getHours()+":"+date.getMinutes() + " - " + logs['hit'][getRandom(logs['hit'].length)-1].replace('[playerKick]',player1.name).replace('[playerDefence]',player2.name) + " -" + value + ` hp ${player2.hp}/100 hp`;
-		case "defence":
-			return date.getHours()+":"+date.getMinutes() + " - " +  logs['defence'][getRandom(logs['defence'].length)-1].replace('[playerKick]',player1.name).replace('[playerDefence]',player2.name);
-		case "end":
-			return date.getHours()+":"+date.getMinutes() + " - " +  logs['end'][getRandom(logs['end'].length)-1].replace('[playerWins]',player1.name).replace('[playerLose]',player2.name);
-		case "draw":
-			return date.getHours()+":"+date.getMinutes() + " - " +  logs['draw'][0];
-		case "start":
-			return logs['start'].replace('[player1]',player1.name).replace('[player2]',player2.name).replace('[time]',date.getHours()+":"+date.getMinutes());
-		default:
-			return ""
-	}
-}
- const player1 = {
-	player:1,
-	name:'Scorpion',
-	hp:100,
-	weapon:'Fire',
-	attack:function(name) {console.log(name + ' Fight...');},
-	img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-	changeHP,
-	renderHP,
-	elHP
-};
- const player2 = {
-	player:2,
-	name:'Sub-Zero',
-	hp:100,
-	weapon:'Ice',
-	attack:function(name) {console.log(name + ' Fight...');},
-	img:'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-	changeHP,
-	renderHP,
-	elHP
-};
- function changeHP(qty) {
-	//console.log(qty);
-	this.hp -= qty;
-	if (this.hp<0){
-		this.hp=0
-	};
-}
- function elHP(player) {
-	const $playerLife = document.querySelector('.player'+ this.player +' .life');
-	
-	return $playerLife
-}
- function renderHP (object){
-	object.style.width = this.hp + '%';
-	
-}
-function showReesult(){
-	if (player1.hp===0||player2.hp===0)
-	{$formFight.disabled = true;
-		$arenas.appendChild(createReloadButton ());
-		appendReloadB();}
+import {renderLogs,enemyAttack,playerAttack, createElement,generateLogs,player1,player2,changeHP,elHP,renderHP,showReesult} from './utils.js'
 
 
-	if (player1.hp===0 && player1.hp < player2.hp){
-		$arenas.appendChild(playerWins(player2.name));
-		renderLogs(generateLogs('end',player2,player1));
-	}
-	else if (player2.hp===0 && player2.hp < player1.hp){
-		$arenas.appendChild(playerWins(player1.name));
-		renderLogs(generateLogs('end',player1,player2));
-	}
-	else if (player1.hp===0 && player2.hp===0){
-		$arenas.appendChild(playerWins());
-		renderLogs(generateLogs('draw'));
-	}
-}
-
-
-
-console.log(player2,player1)
-const $formFight=document.querySelector(".control");
 
 function createPlayer(playerName,playerLife,imgsrc,player) {
 	const $player = createElement('div','player'+player);
@@ -107,17 +27,16 @@ function createPlayer(playerName,playerLife,imgsrc,player) {
 	return $player
 
 };
-const $arenas=document.querySelector(".arenas");
+
 renderLogs(generateLogs('start',player1,player2));
 
 const {name,life,img,player:playerONE,value} = player1;
 const {name:name2,life:life2,img:img2,player:playerTWO,value:value2} = player2;
 
+const $formFight=document.querySelector(".control");
+const $arenas=document.querySelector(".arenas");
 $arenas.appendChild(createPlayer( name,life,img,playerONE));
 $arenas.appendChild(createPlayer(  name2, life2,img2,playerTWO));
-
-
-
 $formFight.addEventListener('submit',function(e){
 	e.preventDefault();
 	//console.dir($formFight);
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 
+import {logs} from './logs.js'
 const HIT = {
     head: 30,
     body: 25,
@@ -72,4 +73,86 @@ export const appendReloadB = () => {
 	window.location.reload();
 	});
 }   
+
+//
+
+export function generateLogs(type,player1,player2,value){
+	const date = new Date();
+	console.log(player2)
+	switch (type){
+		case "hit":
+			return date.getHours()+":"+date.getMinutes() + " - " + logs['hit'][getRandom(logs['hit'].length)-1].replace('[playerKick]',player1.name).replace('[playerDefence]',player2.name) + " -" + value + ` hp ${player2.hp}/100 hp`;
+		case "defence":
+			return date.getHours()+":"+date.getMinutes() + " - " +  logs['defence'][getRandom(logs['defence'].length)-1].replace('[playerKick]',player1.name).replace('[playerDefence]',player2.name);
+		case "end":
+			return date.getHours()+":"+date.getMinutes() + " - " +  logs['end'][getRandom(logs['end'].length)-1].replace('[playerWins]',player1.name).replace('[playerLose]',player2.name);
+		case "draw":
+			return date.getHours()+":"+date.getMinutes() + " - " +  logs['draw'][0];
+		case "start":
+			return logs['start'].replace('[player1]',player1.name).replace('[player2]',player2.name).replace('[time]',date.getHours()+":"+date.getMinutes());
+		default:
+			return ""
+	}
+}
+export const player1 = {
+	player:1,
+	name:'Scorpion',
+	hp:100,
+	weapon:'Fire',
+	attack:function(name) {console.log(name + ' Fight...');},
+	img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+	changeHP,
+	renderHP,
+	elHP
+};
+export const player2 = {
+	player:2,
+	name:'Sub-Zero',
+	hp:100,
+	weapon:'Ice',
+	attack:function(name) {console.log(name + ' Fight...');},
+	img:'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+	changeHP,
+	renderHP,
+	elHP
+};
+export function changeHP(qty) {
+	//console.log(qty);
+	this.hp -= qty;
+	if (this.hp<0){
+		this.hp=0
+	};
+}
+export function elHP(player) {
+	const $playerLife = document.querySelector('.player'+ this.player +' .life');
+	
+	return $playerLife
+}
+export function renderHP (object){
+	object.style.width = this.hp + '%';
+	
+}
+export function showReesult(){
+	if (player1.hp===0||player2.hp===0)
+	{$formFight.disabled = true;
+		$arenas.appendChild(createReloadButton ());
+		appendReloadB();}
+
+
+	if (player1.hp===0 && player1.hp < player2.hp){
+		$arenas.appendChild(playerWins(player2.name));
+		renderLogs(generateLogs('end',player2,player1));
+	}
+	else if (player2.hp===0 && player2.hp < player1.hp){
+		$arenas.appendChild(playerWins(player1.name));
+		renderLogs(generateLogs('end',player1,player2));
+	}
+	else if (player1.hp===0 && player2.hp===0){
+		$arenas.appendChild(playerWins());
+		renderLogs(generateLogs('draw'));
+	}
+}
+
+
+
 
